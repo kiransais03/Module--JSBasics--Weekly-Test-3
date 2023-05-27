7fff1a268d28498063a8276a3312c3171dc70fbc
kiransai <kiransais03@gmail.com>
2023-05-28 00:33:15 +0530

    push4

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let tableboxcont = document.getElementById('tablebox')
-
+let datalistcopy;
 function addDatatoUi(datalist) {
   console.log(datalist);
-  console.log(datalist[0]);
+
+  datalistcopy=datalist;
+
+
   document.getElementById('tablebox').innerHTML="";
   for(let i=0;i<datalist.length;i++)
   {
     let object=datalist[i];
     let division = document.createElement('div');
     division.className ="dataelem";
+    let ratecolor="red"
+    function pricecolor () {
+      if(object[i].price_change_percentage_24h > 0)
+      {
+        ratecolor="green"
+      }
+    }
+    pricecolor();
     let inHTML = 
     `<div class="block">
     <div class="image">
@@ -22,7 +33,7 @@ function addDatatoUi(datalist) {
   <div class="symbol">${object.symbol}</div>
   <div class="current_price">$${object.current_price}</div>
   <div class="total_volume">$${object.total_volume}</div>
-  <div class="price_change_percentage_24h">${object.price_change_percentage_24h}%</div>
+  <div class="price_change_percentage_24h ${ratecolor}">${object.price_change_percentage_24h}%</div>
   <div class="market_cap">Mkt Cap:$${object.market_cap}</div>`
 
   division.innerHTML =inHTML;
@@ -64,23 +75,57 @@ function responseprom_error(errordata) {
 }
 
 
-//Search and Button Filters
+//Search
 // https://api.coingecko.com/api/v3/search?query=${}
 
+const inputbox = document.getElementById('name_symb');
 
-let textinput = document.getElementById("name_symb").value;
+var textval;
 
-async function getnamedata () {
+inputbox.addEventListener('input', () => {
+  textval = inputbox.value;
+  console.log(textval);
+    }
+);
+
+function getnamedata () {
   console.log("entered");
-    try {
-      const nameprom = await fetch('https://api.coingecko.com/api/v3/search?query=${textinput}');
-      const data= await nameprom.json();
+  console.log(textval);
+   for(let i=0;i<datalistcopy.length;i++)
+   {
+    console.log("pr")
+    // console.log(datalistcopy[i].name);
+    // console.log(datalistcopy[i].symbol);
+    if(datalistcopy[i].name==textval || datalistcopy[i].symbol==textval)
+    {
+      let ansobj=datalistcopy[i];
+      let reply=[];
+      reply.push(ansobj);
+      addDatatoUi(reply);
+      console.log("if true");
+      break;
+    }
+   }
+
+   if(textval.length===0) {
+    console.log("okk");
+  async function calldefault () {
+    try{
+      const resp=await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      );
+      const data=await resp.json();
       addDatatoUi(data);
     }
     catch(error) {
-      alert("Some Error Ocurred");
+      alert("Error pls refresh the webpage");
     }
   }
+
+  calldefault();
+}
+  }
+
 
 let searchbutton = document.getElementById("search");
 
